0f2ab679180ca92dcdfafd89fa21c9badb83acca
Student-Josh <Josh.Scott.222@cranfield.ac.uk>
2026-01-19 15:17:29 +0000

    Created Sign Up page and edited the style sheet with new sign up option.

--- includes/SignUp-modal.php
+++ includes/SignUp-modal.php
@@ -8,14 +8,26 @@
             <img src="assets/images/FitLife-logo.png" alt="logo" class="logo">
         </div>
 
-        <div class="container">
+       <div class="container">
+
+            <label for="fn"><b>First Name</b></label>
+            <input type="first name" placeholder="Enter First Name" name="FN" required>
+
+            <label for="ln"><b>Last Name</b></label>
+            <input type="last name" placeholder="Enter last name" name="LN" required>
 
             <label for="uname"><b>Username</b></label>
             <input type="text" placeholder="Enter Username" name="uname" required>
 
             <label for="psw"><b>Password</b></label>
             <input type="password" placeholder="Enter Password" name="psw" required>
+            
+            <label for="eml"><b>Email</b></label>
+            <input type="email" placeholder="Enter email" name="eml" required>
 
+            <label for="mob"><b>Mobile</b></label>
+            <input type="mobile" placeholder="mobile number" name="mob" required>
+            
             <button type="submit">Login</button>
             <label>
                 <input type="checkbox" checked="checked" name="remember"> Remember me
